27479ce7e83c101f5e8bd4c2202bf9e2072583f9
aryadaputra <aryajokers@gmail.com>
2018-03-25 22:41:47 +0800

    layout

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+
+} from 'react-native';
+import Layout from './src/latihan/LatihanLayout/Layout';
+
+
+export default class App extends Component {
+
+  render() {
+    return (
+
+        <Layout />
+
+    );
+  }
+}
